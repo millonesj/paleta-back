4d13ba420f1ae66f69c2dc4b6b7f9376ae3d9f07
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2019-11-19 22:17:03 -0500

    feat(proyects): Add controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProyectsController } from './proyects/proyects.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProyectsController],
   providers: [AppService],
 })
 export class AppModule {}
