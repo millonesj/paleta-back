1d94416ede8b232bba445c2d862ce9f61c40689b
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2019-11-19 22:20:29 -0500

    refactor(proyects): Add HTTP request methods

--- src/proyects/proyects.controller.ts
+++ src/proyects/proyects.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post,Put, Delete, Body, Param } from '@nestjs/common';
 
 @Controller('proyects')
-export class ProyectsController {}
+export class ProyectsController {
+  @Post()
+    createProyect():string {
+      return `Created new Proyect`
+  }
+  @Get()
+  getAllProyects(): string {
+    return `All the Proyects!`;
+  }
+  @Put(':id')
+  editProyect( @Param('id') idProyect: string ): string {
+    return `edited  proyect ${idProyect}`;
+  }
+  @Delete(':id')
+  removeProyect( @Param('id') idProyect: string): string {
+    return `proyect removed ${idProyect}`;
+  }
+}
