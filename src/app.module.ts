d8aaea2347774f82ddc144f5915a1162f0cf3bad
Jhonny Neciosup Millones <jhonny.millones@gmail.com>
2019-11-19 22:35:50 -0500

    feat(proyects): Add service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProyectsController } from './proyects/proyects.controller';
+import { ProyectsService } from './proyects/proyects.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ProyectsController],
-  providers: [AppService],
+  providers: [AppService, ProyectsService],
 })
 export class AppModule {}
